5abe098e8af97e66052653e1667ec1830bb90d56
weissleb <weissleb>
2023-12-22 18:42:56 +0000

    start notebook

---
+++ ELT with Apache Spark.sql
@@ -0,0 +1,13 @@
+-- Databricks notebook source
+-- MAGIC %python
+-- MAGIC dbutils.fs.ls("/")
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC dbutils.fs.ls("databricks-datasets")
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC dbutils.fs.ls("databricks-datasets/songs")
