2b669629be0c34d9048a26231a5bf6ac22f775a7
weissleb <weissleb>
2023-12-22 19:44:13 +0000

    got some data

--- ELT with Apache Spark.sql
+++ ELT with Apache Spark.sql
@@ -10,9 +10,66 @@
 -- COMMAND ----------
 
 -- MAGIC %python
--- MAGIC dbutils.fs.ls("databricks-datasets/songs")
+-- MAGIC dbutils.fs.ls("databricks-datasets/travel_recommendations_realtime")
 
 -- COMMAND ----------
 
 -- MAGIC %python
--- MAGIC dbutils.fs.ls("dbfs:/databricks-datasets/songs/data-001")
+-- MAGIC dbutils.fs.ls("databricks-datasets/travel_recommendations_realtime/raw_travel_data")
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC dbutils.fs.ls("databricks-datasets/travel_recommendations_realtime/raw_travel_data/fs-demo_destination-availability_logs")
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC dbutils.fs.ls("databricks-datasets/travel_recommendations_realtime/raw_travel_data/fs-demo_destination-availability_logs/json")
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC jsonDF = spark.read.json("dbfs:/databricks-datasets/travel_recommendations_realtime/raw_travel_data/fs-demo_destination-availability_logs/json/*.json")
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC jsonDF.show()
+
+-- COMMAND ----------
+
+select *
+from json.`dbfs:/databricks-datasets/travel_recommendations_realtime/raw_travel_data/fs-demo_destination-availability_logs/json/*.json`
+limit 20
+
+-- COMMAND ----------
+
+use catalog weissler;
+
+-- COMMAND ----------
+
+create schema if not exists travel_bronze;
+
+-- COMMAND ----------
+
+-- drop table travel_bronze.availability_logs;
+
+-- COMMAND ----------
+
+create table if not exists travel_bronze.availability_logs using delta;
+describe extended travel_bronze.availability_logs;
+
+-- COMMAND ----------
+
+copy into travel_bronze.availability_logs
+from 'dbfs:/databricks-datasets/travel_recommendations_realtime/raw_travel_data/fs-demo_destination-availability_logs/json/*.json'
+fileformat = JSON
+format_options ('inferSchema' = 'true')
+copy_options ('mergeSchema' = 'true');
+
+-- COMMAND ----------
+
+select *
+from travel_bronze.availability_logs
+limit 20;
